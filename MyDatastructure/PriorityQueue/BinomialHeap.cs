e4709c6137fc10ccfc02d5cce918fac8979a80a7
iluvjava <victor1301166040@gmail.com>
2019-09-09 20:33:22 -0700

    stuff

--- MyDatastructure/PriorityQueue/BinomialHeap.cs
+++ MyDatastructure/PriorityQueue/BinomialHeap.cs
@@ -1,10 +1,55 @@
-﻿using System;
+﻿using MyDatastructure.PriorityQ;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyDatastructure.PriorityQueue
 {
-    class BinomialHeap
+    public class BinomialHeap<T> : IPriorityQ<T> where T : IComparable<T>
     {
+        public int Size => throw new NotImplementedException();
+
+        public bool Contains(T arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveMin()
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
+    public class BNode<T> where T : IComparable<T>
+    {
+        int Rank;
+        T Data;
+        BNode<T>[] Children;
+
+        public BNode(int Rank, T Data)
+        {
+            if (Rank < 0 || object.ReferenceEquals(Data, null))
+                throw new Exception();
+            Children = Rank == 0? null : new BNode<T>[Rank] ;
+        }
+
+
+    }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # C-DataStructures
 A Net. Core Library for C# Datastructure. 
+
+
+
+## Binomial Heap (BinomialHeap.cs)
+
+### The Node
+- Holds data, rank of the tree, left and right child
+  - Data Can not be null, because it's comparable. 
+- For a tree with rank N, it has N subtree, includes the B_0.
+- What does it do? 
+  - B_N merges with B_N. Tree with the same rank merges together, the one with 
+  larger root is connected to the one with smaller root. 
+
+### The Tree
+
+### The Heap
