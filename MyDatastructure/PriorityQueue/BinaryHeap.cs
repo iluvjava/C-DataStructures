1a52b10d42a59fdbd341f9fad068ebb5dc0d3dde
Dashie <32211153+iluvjava@users.noreply.github.com>
2019-09-11 13:52:55 -0700

    new stuff

--- MyDatastructure/PriorityQueue/BinaryHeap.cs
+++ MyDatastructure/PriorityQueue/BinaryHeap.cs
@@ -6,12 +6,103 @@ using static System.Array;
 namespace MyDatastructure.PriorityQueue
 {
     /// <summary>
-    /// This will be the simplest Binary Heap imaginable.
+    /// Simple binary heap that uses a hash map to keep track of the element
+    /// , supports unique elements.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [Obsolete("Never tested yet.")]
+    public class FancierBinaryHeap<T> : SimpleBinaryHeap<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// maps the object to index in the heap array.
+        /// </summary>
+        protected IMap<T, int> IndexMap;
+
+        public FancierBinaryHeap() : base()
+        {
+            IndexMap = new SysDefaultMap<T, int>();
+        }
+
+        public FancierBinaryHeap(bool asc) : base(asc)
+        {
+            IndexMap = new SysDefaultMap<T, int>();
+        }
+
+        /// <summary>
+        /// Floyd Build Heap on this.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="offset"></param>
+        /// <param name="len"></param>
+        protected FancierBinaryHeap(T[] arr, int offset, int len) : base(arr, offset, len)
+        {
+        }
+
+        public bool Contain(T arg)
+        {
+            return IndexMap.ContainsKey(arg);
+        }
+
+        /// <summary>
+        /// you cannot add duplicated element to the heap.
+        /// </summary>
+        /// <param name="arg"></param>
+        public void Enqueue(T arg)
+        {
+            if (IsNull(arg))
+                throw new InvalidArgumentException();
+            if (IndexMap.ContainsKey(arg))
+            {
+                throw new InvalidArgumentException();
+            }
+            IndexMap[arg] = ElementCount + 1;
+            base.Enqueue(arg);
+        }
+
+        public void Remove(T arg)
+        {
+            if (!IndexMap.ContainsKey(arg))
+            {
+                throw new InvalidArgumentException();
+            }
+            int IndexOfRemove = IndexMap[arg];
+            Swap(IndexOfRemove, ElementCount);
+            IndexMap.Remove(arg);
+            ElementCount--;
+            Percolate(IndexOfRemove);
+        }
+
+        public void RemoveMin()
+        {
+            if (ElementCount == 0)
+                throw new InvalidOperationException();
+            T Removed = Peek();
+            base.RemoveMin();
+            IndexMap.Remove(Removed);
+        }
+
+        protected void Swap(int arg1, int arg2)
+        {
+            T a = HeapArray[arg1];
+            T b = HeapArray[arg2];
+            IndexMap[a] = arg2;
+            IndexMap[b] = arg1;
+            base.Swap(arg1, arg2);
+        }
+    }
+
+    /// <summary>
+    /// This will be the simplest Binary Heap imaginable, Min or Max.
     /// 0 index will be a dummy
     /// Null is not a accepted value.
     /// </summary>
     public class SimpleBinaryHeap<T> : IPriorityQ<T> where T : IComparable<T>
     {
+        /// <summary>
+        /// A boolean to indicated the ordering.
+        /// </summary>
+        protected bool AscendingOrder = true;
+
         protected int ElementCount = 0;
 
         /// <summary>
@@ -27,36 +118,40 @@ namespace MyDatastructure.PriorityQueue
             HeapArray = new T[32];
         }
 
+        public SimpleBinaryHeap(bool ascending) : this()
+        {
+            AscendingOrder = false;
+        }
+
         /// <summary>
-        /// Give an array of element it will build the heap from it. 
-        /// It will copy the array. 
+        /// Give an array of element it will build the heap from it.
+        /// It will copy the array.
         /// <remark>
-        /// It uses floyd build heap, good for partial sort. 
+        /// It uses floyd build heap, good for partial sort.
         /// </remark>
         /// </summary>
         /// <param name="list">
         /// The list should not comtain null, it will blows up if it contains
         /// null.
         /// <param name="offset">
-        /// Where you want to start add element from the array to the heap. 
-        /// Invalid argument will be set to 0. 
-        /// It's default to be 0. 
+        /// Where you want to start add element from the array to the heap.
+        /// Invalid argument will be set to 0.
+        /// It's default to be 0.
         /// </param>
         /// <param name="len">
-        /// The length you want off from the offset you set.  
+        /// The length you want off from the offset you set.
         /// </param>
         public SimpleBinaryHeap(T[] source, int offset = 0, int len = -1)
         {
-            len = (len == -1 || len + offset >= source.Length) ? source.Length - offset: len;
+            len = (len == -1 || len + offset >= source.Length) ? source.Length - offset : len;
             offset = (offset >= source.Length || offset < 0) ? 0 : offset;
-            HeapArray = new T[len*2];
+            HeapArray = new T[len * 2];
             Copy(source, offset, HeapArray, 1, len);
             ElementCount = len;
             for (int i = len / 2; i >= 1; i--)
             {
                 PercolateDown(i);
             }
-            
         }
 
         public int Size
@@ -92,7 +187,7 @@ namespace MyDatastructure.PriorityQueue
                 throw new InvalidArgumentException();
             AutomaticResize();
             HeapArray[ElementCount + 1] = arg;
-            PercolateUp(ElementCount+ 1);
+            PercolateUp(ElementCount + 1);
             ElementCount++;
         }
 
@@ -115,7 +210,7 @@ namespace MyDatastructure.PriorityQueue
         /// </summary>
         /// <throw>
         /// InvalidOperationException is the heap is empty when
-        /// trying to remove elements. 
+        /// trying to remove elements.
         /// </throw>
         /// <returns></returns>
         public T RemoveMin()
@@ -135,6 +230,21 @@ namespace MyDatastructure.PriorityQueue
             {
                 Resize(ref HeapArray, HeapArray.Length * 2);
             }
+        }
+
+        /// <summary>
+        /// Take in account into the settings of the heap to compare the
+        /// element.
+        /// <code>
+        /// a.CompareTo(b) if ascending
+        /// - a.CompareTo(b) if descending.
+        /// </code>
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        protected int Compare(T a, T b)
+        {
+            return AscendingOrder ? a.CompareTo(b) : -a.CompareTo(b);
         }
 
         protected int GetFirstChildIndex(int arg)
@@ -173,9 +283,10 @@ namespace MyDatastructure.PriorityQueue
             {
                 T LChild = HeapArray[LeftChildIdx];
                 T RChild = HeapArray[RightChildIdx];
-                int TheSmallerChildIdx = 
-                    LChild.CompareTo(RChild) < 0 ? LeftChildIdx : RightChildIdx;
-                if (HeapArray[TheSmallerChildIdx].CompareTo(Parent) < 0)
+                int TheSmallerChildIdx =
+                    Compare(LChild, RChild) < 0 ? LeftChildIdx : RightChildIdx;
+
+                if (Compare(HeapArray[TheSmallerChildIdx], Parent) < 0)
                 {
                     Swap(TheSmallerChildIdx, arg);
                     return PercolateDown(TheSmallerChildIdx);
@@ -186,7 +297,7 @@ namespace MyDatastructure.PriorityQueue
             if (LeftChildIdx <= ElementCount)
             {
                 T LChild = HeapArray[LeftChildIdx];
-                if (LChild.CompareTo(Parent) < 0)
+                if (Compare(LChild, Parent) < 0)
                 {
                     Swap(LeftChildIdx, arg);
                     return PercolateDown(LeftChildIdx);
@@ -204,7 +315,7 @@ namespace MyDatastructure.PriorityQueue
             int Pidx = GetParentIndex(arg);
             T Parent = HeapArray[Pidx];
             T Child = HeapArray[arg];
-            if (Child.CompareTo(Parent) < 0)
+            if (Compare(Child, Parent) < 0)
             {
                 Swap(Pidx, arg);
                 return PercolateUp(Pidx);
@@ -221,87 +332,5 @@ namespace MyDatastructure.PriorityQueue
             HeapArray[arg1] = b;
             HeapArray[arg2] = a;
         }
-    }
-
-    /// <summary>
-    /// Simple binary heap that uses a hash map to keep track of the element
-    /// , supports unique elements. 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    [Obsolete("Never tested yet.")]
-    public class FancierBinaryHeap<T> : SimpleBinaryHeap<T> where T : IComparable<T>
-    {
-        /// <summary>
-        /// maps the object to index in the heap array. 
-        /// </summary>
-        protected IMap<T, int> IndexMap;
-
-        public FancierBinaryHeap() : base()
-        {
-            IndexMap = new SysDefaultMap<T, int>();
-        }
-
-        protected FancierBinaryHeap(T[] arr, int offset, int len) : base(arr, offset, len)
-        {
-
-        }
-
-
-        /// <summary>
-        /// you cannot add duplicated element to the heap. 
-        /// </summary>
-        /// <param name="arg"></param>
-        public void Enqueue(T arg)
-        {
-            if (IsNull(arg))
-                throw new InvalidArgumentException();
-            if (IndexMap.ContainsKey(arg))
-            {
-                throw new InvalidArgumentException();
-            }
-            IndexMap[arg] = ElementCount + 1;
-            base.Enqueue(arg);
-        }
-
-        protected void Swap(int arg1, int arg2)
-        {
-            T a = HeapArray[arg1];
-            T b = HeapArray[arg2];
-            IndexMap[a] = arg2;
-            IndexMap[b] = arg1;
-            base.Swap(arg1, arg2);
-        }
-
-        public void RemoveMin()
-        {
-            if (ElementCount == 0)
-                throw new InvalidOperationException();
-            T Removed = Peek();
-            base.RemoveMin();
-            IndexMap.Remove(Removed);
-        }
-
-
-        public bool Contain(T arg)
-        {
-            return IndexMap.ContainsKey(arg);
-        }
-
-        public void Remove(T arg)
-        {
-            if (!IndexMap.ContainsKey(arg))
-            {
-                throw new InvalidArgumentException();
-            }
-            int IndexOfRemove = IndexMap[arg];
-            Swap(IndexOfRemove, ElementCount);
-            IndexMap.Remove(arg);
-            ElementCount--;
-            Percolate(IndexOfRemove);
-        }
-
-
-
-
     }
 }
---
+++ MyDatastructure/Sorting/TopAndBottomSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDatastructure.Sorting
+{
+
+    /// <summary>
+    /// A better version of Top K sort, it find the top and bottom N-k, k element 
+    /// from a list of comparable, it will also remember the min and the max too, 
+    /// on average, close to O(N log(K)) performace. 
+    /// 
+    /// It will not modified the original structure. 
+    /// </summary>
+    class TopAndBottomSort<T> where T: IComparable<T>
+    {
+        T[] UnoderedData;
+        T Max { get; }
+        T Min { get; }
+        T MinK { get; }
+        T MaxK { get; }
+        /// <summary>
+        /// All elements less than the K th max element and 
+        /// larger than the Kth element. 
+        /// </summary>
+        T[] MiddleBody;
+
+
+
+        /// <summary>
+        /// Only Support K, on number, symmetric. 
+        /// It will not modify the array. 
+        /// </summary>
+        /// <param name="K">
+        /// The number of top and bottom you want from the data set. 
+        /// </param>
+        /// <param name="arr">
+        /// The random number of comparable you prepared.   
+        /// </param>
+        public TopAndBottomSort(int K, T[] arr)
+        {
+            if (K <= 1 || 2*K >= arr.Length)
+            {
+                throw new Exception();
+            }
+
+        }
+
+    }
+}
